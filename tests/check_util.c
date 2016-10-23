31f6d0ec4423eac3268f0b94cdca3a98d34622ff
Adam Goode <agoode@google.com>
2016-10-23 10:01:41 -0400

    Add tests for verify_name_allowed in MINIMAL mode

--- tests/check_util.c
+++ tests/check_util.c
@@ -17,8 +17,54 @@
   USA.
 ***/
 
+#include <check.h>
+#include <stdlib.h>
+#include "../src/util.h"
 
+// Tests that verify_name_allowed works in MINIMAL mode.
+// Only names with TLD "local" are allowed.
+START_TEST(test_verify_name_minimal) {
+    // Positive results.
+    ck_assert(verify_name_allowed("foo.local", NULL));
+    ck_assert(verify_name_allowed("foo.local.", NULL));
+
+    // Negative results.
+    ck_assert(!verify_name_allowed("foo.com", NULL));
+    ck_assert(!verify_name_allowed("foo.com.", NULL));
+
+    ck_assert(!verify_name_allowed("foo.local.com", NULL));
+    ck_assert(!verify_name_allowed("foo.local.com.", NULL));
+
+    ck_assert(!verify_name_allowed("", NULL));
+    ck_assert(!verify_name_allowed(".", NULL));
+}
+END_TEST
+
+
+// Boilerplate from https://libcheck.github.io/check/doc/check_html/check_3.html
+static Suite *util_suite(void) {
+    Suite *s;
+    TCase *tc_core;
+
+    s = suite_create("util");
+    tc_core = tcase_create("Core");
+
+    tcase_add_test(tc_core, test_verify_name_minimal);
+    suite_add_tcase(s, tc_core);
+
+    return s;
+}
 
 int main(void) {
-  return 0;
+    int number_failed;
+    Suite *s;
+    SRunner *sr;
+
+    s = util_suite();
+    sr = srunner_create(s);
+
+    srunner_run_all(sr, CK_VERBOSE);
+    number_failed = srunner_ntests_failed(sr);
+    srunner_free(sr);
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
