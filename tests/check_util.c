f4a19ab9b4e8961b7783f675a24c6f0cf6f5fb9c
Adam Goode <agoode@google.com>
2016-10-23 14:39:03 -0400

    Add many more tests for `verify_name_allowed`

--- tests/check_util.c
+++ tests/check_util.c
@@ -17,29 +17,197 @@
   USA.
 ***/
 
+#define _POSIX_C_SOURCE 200809L
+
 #include <check.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "../src/util.h"
 
-// Tests that verify_name_allowed works in MINIMAL mode.
+// Tests that verify_name_allowed works in MINIMAL mode, or with no config file.
 // Only names with TLD "local" are allowed.
-START_TEST(test_verify_name_minimal) {
-    // Positive results.
-    ck_assert(verify_name_allowed("foo.local", NULL));
-    ck_assert(verify_name_allowed("foo.local.", NULL));
-
-    // Negative results.
-    ck_assert(!verify_name_allowed("foo.com", NULL));
-    ck_assert(!verify_name_allowed("foo.com.", NULL));
-
-    ck_assert(!verify_name_allowed("foo.local.com", NULL));
-    ck_assert(!verify_name_allowed("foo.local.com.", NULL));
-
+START_TEST(test_verify_name_allowed_minimal) {
+    ck_assert(verify_name_allowed("example.local", NULL));
+    ck_assert(verify_name_allowed("example.local.", NULL));
+    ck_assert(verify_name_allowed("com.example.local", NULL));
+    ck_assert(verify_name_allowed("com.example.local.", NULL));
+    ck_assert(!verify_name_allowed("example.com", NULL));
+    ck_assert(!verify_name_allowed("example.com.", NULL));
+    ck_assert(!verify_name_allowed("example.local.com", NULL));
+    ck_assert(!verify_name_allowed("example.local.com.", NULL));
     ck_assert(!verify_name_allowed("", NULL));
     ck_assert(!verify_name_allowed(".", NULL));
 }
 END_TEST
 
+
+// Calls verify_name_allowed by first creating a memfile to read from.
+static int verify_name_allowed_from_string(const char *name,
+                                           const char *file_contents) {
+    FILE *f = fmemopen((void *) file_contents, strlen(file_contents), "r");
+    int result = verify_name_allowed(name, f);
+    fclose(f);
+    return result;
+}
+
+// Tests verify_name_allowed with empty config.
+// Nothing is permitted.
+START_TEST(test_verify_name_allowed_empty) {
+    const char allow_file[] = "";
+
+    ck_assert(!verify_name_allowed_from_string("example.local", allow_file));
+    ck_assert(!verify_name_allowed_from_string("example.local.", allow_file));
+    ck_assert(!verify_name_allowed_from_string("com.example.local", allow_file));
+    ck_assert(!verify_name_allowed_from_string("com.example.local.",
+                                               allow_file));
+    ck_assert(!verify_name_allowed_from_string("example.com", allow_file));
+    ck_assert(!verify_name_allowed_from_string("example.com.", allow_file));
+    ck_assert(!verify_name_allowed_from_string("example.local.com",
+                                               allow_file));
+    ck_assert(!verify_name_allowed_from_string("example.local.com.",
+                                               allow_file));
+    ck_assert(!verify_name_allowed_from_string("", allow_file));
+    ck_assert(!verify_name_allowed_from_string(".", allow_file));
+}
+END_TEST
+
+// Tests verify_name_allowed with the standard config.
+START_TEST(test_verify_name_allowed_default) {
+    const char allow_file[] =
+        "# /etc/mdns.allow\n"
+        ".local.\n"
+        ".local\n";
+
+    ck_assert(verify_name_allowed_from_string("example.local", allow_file));
+    ck_assert(verify_name_allowed_from_string("example.local.", allow_file));
+    ck_assert(verify_name_allowed_from_string("com.example.local", allow_file));
+    ck_assert(verify_name_allowed_from_string("com.example.local.", allow_file));
+    ck_assert(!verify_name_allowed_from_string("example.com", allow_file));
+    ck_assert(!verify_name_allowed_from_string("example.com.", allow_file));
+    ck_assert(!verify_name_allowed_from_string("example.local.com",
+                                               allow_file));
+    ck_assert(!verify_name_allowed_from_string("example.local.com.",
+                                               allow_file));
+    ck_assert(!verify_name_allowed_from_string("", allow_file));
+    ck_assert(!verify_name_allowed_from_string(".", allow_file));
+}
+END_TEST
+
+// Tests verify_name_allowed with wildcard.
+START_TEST(test_verify_name_allowed_wildcard) {
+    const char allow_file[] = "*\n";
+
+    ck_assert(verify_name_allowed_from_string("example.local", allow_file));
+    ck_assert(verify_name_allowed_from_string("example.local.", allow_file));
+    ck_assert(verify_name_allowed_from_string("com.example.local", allow_file));
+    ck_assert(verify_name_allowed_from_string("com.example.local.", allow_file));
+    ck_assert(verify_name_allowed_from_string("example.com", allow_file));
+    ck_assert(verify_name_allowed_from_string("example.com.", allow_file));
+    ck_assert(verify_name_allowed_from_string("example.local.com",
+                                              allow_file));
+    ck_assert(verify_name_allowed_from_string("example.local.com.",
+                                              allow_file));
+    ck_assert(verify_name_allowed_from_string("", allow_file));
+    ck_assert(verify_name_allowed_from_string(".", allow_file));
+}
+END_TEST
+
+// Tests verify_name_allowed with too-long lines.
+START_TEST(test_verify_name_allowed_too_long) {
+    const char allow_file[] =
+        "# /etc/mdns.allow\n"
+        ".local."
+        "                                                  " // 50 spaces
+        "                                                  " // 50 spaces
+        "                                                  " // 50 spaces
+        "\n"
+        ".local\n";
+
+    ck_assert(verify_name_allowed_from_string("example.local", allow_file));
+    ck_assert(verify_name_allowed_from_string("example.local.", allow_file));
+    ck_assert(verify_name_allowed_from_string("com.example.local", allow_file));
+    ck_assert(verify_name_allowed_from_string("com.example.local.", allow_file));
+    ck_assert(!verify_name_allowed_from_string("example.com", allow_file));
+    ck_assert(!verify_name_allowed_from_string("example.com.", allow_file));
+    ck_assert(!verify_name_allowed_from_string("example.local.com",
+                                               allow_file));
+    ck_assert(!verify_name_allowed_from_string("example.local.com.",
+                                               allow_file));
+    ck_assert(!verify_name_allowed_from_string("", allow_file));
+    ck_assert(!verify_name_allowed_from_string(".", allow_file));
+}
+END_TEST
+
+// Tests verify_name_allowed with too-long non-empty lines.
+START_TEST(test_verify_name_allowed_too_long2) {
+    const char allow_file[] =
+        "# /etc/mdns.allow\n"
+        ".aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" // 50 characters
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" // 50 characters
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" // 50 characters
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" // 50 characters
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" // 50 characters
+        "\n"
+        ".local.\n"
+        ".local\n";
+
+    // The input is truncated at 127 bytes, so we allow this string.
+    ck_assert(verify_name_allowed_from_string(
+        "example"
+        ".aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" // 50 characters
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" // 50 characters
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaa",                       // 27 characters
+        allow_file));
+
+    // Even though this exactly matches the item in the allow file,
+    // it is too long.
+    ck_assert(!verify_name_allowed_from_string(
+        "example"
+        ".aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" // 50 characters
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" // 50 characters
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" // 50 characters
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" // 50 characters
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", // 50 characters
+        allow_file));
+
+    ck_assert(verify_name_allowed_from_string("example.local", allow_file));
+    ck_assert(verify_name_allowed_from_string("example.local.", allow_file));
+    ck_assert(verify_name_allowed_from_string("com.example.local", allow_file));
+    ck_assert(verify_name_allowed_from_string("com.example.local.", allow_file));
+    ck_assert(!verify_name_allowed_from_string("example.com", allow_file));
+    ck_assert(!verify_name_allowed_from_string("example.com.", allow_file));
+    ck_assert(!verify_name_allowed_from_string("example.local.com",
+                                               allow_file));
+    ck_assert(!verify_name_allowed_from_string("example.local.com.",
+                                               allow_file));
+    ck_assert(!verify_name_allowed_from_string("", allow_file));
+    ck_assert(!verify_name_allowed_from_string(".", allow_file));
+}
+END_TEST
+
+// Tests verify_name_allowed with a custom config.
+START_TEST(test_verify_name_allowed_com_and_local) {
+    const char allow_file[] =
+        "# /etc/mdns.allow\n"
+        ".com.\n"
+        ".com\n"
+        ".local.\n"
+        ".local\n";
+
+    ck_assert(verify_name_allowed_from_string("example.local", allow_file));
+    ck_assert(verify_name_allowed_from_string("example.local.", allow_file));
+    ck_assert(verify_name_allowed_from_string("com.example.local", allow_file));
+    ck_assert(verify_name_allowed_from_string("com.example.local.", allow_file));
+    ck_assert(verify_name_allowed_from_string("example.com", allow_file));
+    ck_assert(verify_name_allowed_from_string("example.com.", allow_file));
+    ck_assert(verify_name_allowed_from_string("example.local.com",
+                                              allow_file));
+    ck_assert(verify_name_allowed_from_string("example.local.com.",
+                                              allow_file));
+    ck_assert(!verify_name_allowed_from_string("", allow_file));
+    ck_assert(!verify_name_allowed_from_string(".", allow_file));
+}
+END_TEST
 
 // Boilerplate from https://libcheck.github.io/check/doc/check_html/check_3.html
 static Suite *util_suite(void) {
@@ -49,7 +217,13 @@ static Suite *util_suite(void) {
     s = suite_create("util");
     tc_core = tcase_create("Core");
 
-    tcase_add_test(tc_core, test_verify_name_minimal);
+    tcase_add_test(tc_core, test_verify_name_allowed_minimal);
+    tcase_add_test(tc_core, test_verify_name_allowed_default);
+    tcase_add_test(tc_core, test_verify_name_allowed_empty);
+    tcase_add_test(tc_core, test_verify_name_allowed_wildcard);
+    tcase_add_test(tc_core, test_verify_name_allowed_too_long);
+    tcase_add_test(tc_core, test_verify_name_allowed_too_long2);
+    tcase_add_test(tc_core, test_verify_name_allowed_com_and_local);
     suite_add_tcase(s, tc_core);
 
     return s;
