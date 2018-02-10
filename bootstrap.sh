176937af796fae8ac94521639fb66f8be24c183f
Adam Goode <agoode@google.com>
2018-02-09 23:33:00 -0500

    Update FSF address in source files
    Fixes #25.

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,8 +12,8 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 # General Public License for more details.
 #
-# You should have received a copy of the GNU Lesser General Public License
-# along with nss-mdns; if not, write to the Free Software Foundation,
-# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+# You should have received a copy of the GNU Lesser General Public
+# License along with nss-mdns; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 autoreconf -i -f -v -m
