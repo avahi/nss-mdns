7055e58f2de3829d5acf9bcba7388092234049e0
Adam Goode <agoode@google.com>
2018-02-10 19:42:19 -0500

    Add getaddrinfo support to nss-test.c
    This allows the little program to test the 3 main codepaths:
    - gethostbyname3_r (gethostbyname)
    - gethostbyname4_r (getaddrinfo)
    - gethostbyaddr_r (gethostbyaddr)

--- src/nss-test.c
+++ src/nss-test.c
@@ -24,20 +24,73 @@
 #include <netdb.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <string.h>
 #include <arpa/inet.h>
 
-int main(int argc, char* argv[]) {
+static int gai(const char* node) {
+    struct addrinfo hints;
+    struct addrinfo *result, *rp;
+
+    char str[INET_ADDRSTRLEN];
+    char str6[INET6_ADDRSTRLEN];
+
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_flags = AI_CANONNAME;
+    hints.ai_socktype = SOCK_STREAM;
+
+    fprintf(stderr, "* doing node lookup with getaddrinfo...\n");
+
+    int s = getaddrinfo(node, NULL, &hints, &result);
+    if (s != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+        return 1;
+    }
+    int i = 0;
+    for (rp = result; rp != NULL; rp = rp->ai_next) {
+        if (rp->ai_canonname) {
+            fprintf(stderr, "[%d] official name: %s\n", i, rp->ai_canonname);
+        }
+        switch (rp->ai_family) {
+        case AF_INET:
+            inet_ntop(AF_INET, &((struct sockaddr_in*) rp->ai_addr)->sin_addr, str, sizeof(str));
+            fprintf(stderr, "[%d] addr type: inet\n[%d] address: %s\n", i, i, str);
+            break;
+        case AF_INET6:
+            inet_ntop(AF_INET6, &((struct sockaddr_in6*) rp->ai_addr)->sin6_addr, str6, sizeof(str6));
+            int scope_id = ((struct sockaddr_in6*) rp->ai_addr)->sin6_scope_id;
+            if (scope_id) {
+                fprintf(stderr, "[%d] addr type: inet6\n[%d] address: %s%%%d\n", i, i, str6, scope_id);
+            } else {
+                fprintf(stderr, "[%d] addr type: inet6\n[%d] address: %s\n", i, i, str6);
+            }
+            break;
+        }
+        fprintf(stderr, "\n");
+        i++;
+    }
+    freeaddrinfo(result);
+
+    return 0;
+}
+
+static int gethostbyX(const char* node) {
     struct hostent* he;
     in_addr_t** a;
-    const char* arg = argc > 1 ? argv[1] : "cocaine.local";
     uint8_t t[256];
 
-    if (inet_pton(AF_INET, arg, &t) > 0)
+    if (inet_pton(AF_INET, node, &t) > 0) {
+        fprintf(stderr, "* doing ipv4 lookup with gethostbyaddr...\n");
         he = gethostbyaddr(t, 4, AF_INET);
-    else if (inet_pton(AF_INET6, arg, &t) > 0)
+    }
+    else if (inet_pton(AF_INET6, node, &t) > 0) {
+        fprintf(stderr, "* doing ipv6 lookup with gethostbyaddr...\n");
         he = gethostbyaddr(t, 16, AF_INET6);
-    else
-        he = gethostbyname(arg);
+    }
+    else {
+        fprintf(stderr, "* doing name lookup with gethostbyname...\n");
+        he = gethostbyname(node);
+    }
 
     if (!he) {
         fprintf(stderr, "lookup failed\n");
@@ -66,5 +119,17 @@ int main(int argc, char* argv[]) {
     }
     fprintf(stderr, "\n");
 
+    return 0;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Requires 1 argument: either a host or numeric address\n");
+        return 1;
+    }
+    const char* node = argv[1];
+    gethostbyX(node);
+    fprintf(stderr, "\n\n");
+    gai(node);
     return 0;
 }
